0aab1b7d003ae285a6fd29ce5f9201862409760c
Александра Осмакова <aleksandra.osmakova@MacBook-Pro-Aleksandra.local>
2018-10-18 13:30:34 +0300

    burger menu is added

--- tiger.js/tiger.js
+++ tiger.js/tiger.js
@@ -11,3 +11,20 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
   }); 
+
+var menu__icon = document.getElementsByClassName('menu__icon')[0];
+var menu = document.getElementsByClassName('header_menu_open')[0];
+var burger_title = document.getElementsByClassName('burger_menu_title')[0];
+
+menu__icon.addEventListener('click', menu_open);
+
+function menu_open() {
+    menu.classList.toggle('menu_visible');
+}
+
+var header_burger_menu = document.getElementsByClassName("header_burger_menu")[0];
+
+menu__icon.addEventListener('click', function () {
+    header_burger_menu.classList.toggle('menu_state_open');
+    burger_title.classList.toggle('menu_state_open');
+})
