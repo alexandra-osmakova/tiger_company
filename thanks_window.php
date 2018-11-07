87c29cad20202beae80c95612d00a49530bdbcad
Александра Осмакова <aleksandra.osmakova@MacBook-Pro-Aleksandra.local>
2018-11-07 19:02:07 +0300

    added new sending form

--- thanks_window.php
+++ thanks_window.php
@@ -1,106 +1,16 @@
 <?php 
     header('Refresh: 5; url=index.html', true, 301);
 
-    $quiz_btn = $_POST['quiz_btn'];
-    $modal_btn = $_POST['modal_btn'];
-
     $user_name = $_POST['name'];
     $telephone = $_POST['phone-number'];
-    $apply_task = $_POST['apply_task'];
 
-    $parcel_type = $_POST['parcel_type'];
-
-    $cargo_help = $_POST['cargohelp'];
-    $adding = $_POST['adding'];
-    $gazel = $_POST['gazel'];
-    $three_ton = $_POST['3ton'];
-    $five_ton = $_POST['5ton'];
-    $ten_ton = $_POST['10ton'];
-    $fura = $_POST['fura'];
-    $unusual_shape = $_POST['unusual_shape'];
-
-    $box = $_POST['box'];
-    $palet = $_POST['palet'];
-    $case = $_POST['case'];
-    $bag = $_POST['bag'];
-    $big_bag = $_POST['big_bag'];
-    $obreshetka = $_POST['obreshetka'];
-    $bunch = $_POST['bunch'];
-    $other = $_POST['other'];
-    $no_wrap = $_POST['no'];
-
-    $top_loading = $_POST['top_loading'];
-    $side_loading = $_POST['side_loading'];
-    $back_loading = $_POST['back_loading'];
-    $combined = $_POST['combined'];
-    $no_matter = $_POST['no_matter'];
-
-    $parcel_from_quiz = $_POST['parcel_from_quiz'];
-    $parcel_to_quiz = $_POST['parcel_to_quiz'];
-
-    $asap = $_POST['asap'];
-    $one_day = $_POST['one_day'];
-    $in_week = $_POST['in_week'];
-    $in_month = $_POST['in_month'];
-
-    $person = $_POST['person'];
-    $company = $_POST['company'];
-    $businessman = $_POST['businessman'];
-    $OOO = $_POST['OOO'];
-    echo $modal_btn+'here';
-
-        $email_body = "User Name: $user_name.\n".
-        "User Telephon Number: $telephone.\n".
-        $email_body = 
-            "User Telephon Number: $telephone.\n".
-            "Parcel type: $parcel_type.\n".
-
-            "User needs help: $cargo_help.\n".
-
-            "Dogruz: $adding.\n".
-            "Gazel: $gazel.\n".
-            "3-ton: $three_ton.\n".
-            "5-ton: $five_ton.\n".
-            "10-ton: $ten_ton.\n".
-            "Fura: $fura.\n".
-            "Ne gabarit: $unusual_shape.\n".
-
-            "Box: $box.\n".
-            "Palet: $palet.\n".
-            "Korobka: $case.\n".
-            "Bag: $bag.\n".
-            "Big_bag: $big_bag.\n".
-            "Obreshetka: $obreshetka.\n".
-            "Svyazka: $bunch.\n".
-            "Other: $other.\n".
-            "Bez obertki: $no_wrap.\n".
-
-            "Sverhu: $top_loading.\n".
-            "Sboku: $side_loading.\n".
-            "Szadi: $back_loading.\n".
-            "Combi: $combined.\n".
-            "Ne vazno: $no_matter.\n".
-
-            "Parcel from: $parcel_from_quiz.\n".
-            "Parcel to: $parcel_to_quiz.\n".
-
-            "Bistro: $asap.\n".
-            "1 day: $one_day.\n".
-            "1 week: $in_week.\n".
-            "1 month: $in_month.\n".
-
-            "Person: $person.\n".
-            "Company: $company.\n".
-            "Businessman: $businessman.\n".
-            "OOO: $OOO.\n".
-    
-
-
+    $email_body = "User Name: $user_name.\n".
+    "User Telephon Number: $telephone.\n".
     $email_subject = "New Form Submission";
 
     $to = "alexandra.osmakova@gmail.com";
 
-    $ok = mail($to, $email_subject, $email_body, $headers;
+    $ok = mail($to, $email_subject, $email_body, $headers);
 ?>
 
 <!DOCTYPE html>
