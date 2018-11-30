5b72a5087312ade62fef5c075c9ae42f24a5a8fa
Azamat Mengliev <mengliev.azamat@gmail.com>
2018-11-30 16:27:56 +0300

    Added two layered DB creation

---
+++ admin/admin/create.page.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+
+header('Refresh: 5; url=index.php', true, 301);
+
+require "../database/db.php";
+
+$page = R::dispense("pages");
+
+$uploaddir = '../../tiger.img/';
+
+if (isset($_SESSION["user"]) && $_SESSION["user"] === "admin") {
+    if (isset($_POST["add_page"])) {
+
+        $file_path_first = $uploaddir . $_FILES['img_first']['name'];
+        $file_path_second = $uploaddir . $_FILES['img_second']['name'];
+
+        move_uploaded_file($_FILES['img_first']['tmp_name'], $file_path_first);
+        move_uploaded_file($_FILES['img_second']['tmp_name'], $file_path_second);
+
+        $url_first = "../tiger.img/" . $_FILES['img_first']['name'];
+        $url_second = "../tiger.img/" . $_FILES['img_second']['name'];
+
+        $page->title = $_POST["title"];
+        $page->offer = $_POST["offer"];
+        $page->img_first = $url_first;
+        $page->img_second = $url_second;
+        $page->text_title = $_POST["text_title"];
+        $page->content = $_POST["content"];
+        $page->tag = $_POST["tag"];
+        $page->text = $_POST["text"];
+        $page->description = $_POST["description"];
+
+        R::store($page);
+
+        echo "<div><h1 style='color: green'>Успешно</h1></div>";
+
+    } else {
+        echo "<div><h1 style='color: green'>Что-то пошло не так</h1>></div>";
+    }
+}else {
+    echo "<h1>YOU DON'T HAVE ACCESS TO THIS PAGE</h1>";
+}
